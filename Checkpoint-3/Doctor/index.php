a116e7fde31c358bb91053adf9f6ea654d68a437
Seragis Salekin <52543026+SeragisSalekin@users.noreply.github.com>
2019-12-14 01:47:30 +0600

    Add files via upload

---
+++ Checkpoint-3/Doctor/index.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+include("include/config.php");
+error_reporting(0);
+if(isset($_POST['submit']))
+{
+$ret=mysqli_query($con,"SELECT * FROM doctors WHERE docEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$num=mysqli_fetch_array($ret);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Doctor Login</title>
+		<meta charset="utf-8" />
+	</head>
+	<body class="login">
+		<div class="row">
+			<div class="main-login">
+				<div class="logo margin-top-30">
+				<a href="../../index.html">	<h2> ISOSCELES | Doctor Login</h2></a>
+				</div>
+
+				<div class="box-login">
+					<form class="form-login" method="post">
+						<fieldset>
+							<legend>
+								Sign in to your account
+							</legend>
+							<p>
+								Please enter your name and password to log in.<br />
+								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="text" class="form-control" name="username" placeholder="Username">
+									<i class="fa fa-user"></i> </span>
+							</div>
+							<div class="form-group form-actions">
+								<span class="input-icon">
+									<input type="password" class="form-control password" name="password" placeholder="Password">
+									<i class="fa fa-lock"></i>
+									 </span>
+									 <a href="forgot-password.php">
+									Forgot Password ?
+								</a>
+							</div>
+							<div class="form-actions">
+								
+								<button type="submit" class="btn btn-primary pull-right" name="submit">
+									Login <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+							
+						
+						</fieldset>
+					</form>
+			
+				</div>
+
+			</div>
+		</div>s
+	</body>
+	<!-- end: BODY -->
+</html>
---
+++ Checkpoint-3/SQL File/hms.sql
@@ -0,0 +1,79 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
+) 
+
+INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
+(1, 'admin', 'Test@12345', '28-09-2019 11:42:05 AM');
+
+-- --------------------------------------------------------
+
+--
+
+CREATE TABLE `tblcontactus` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contactno` bigint(12) DEFAULT NULL,
+  `message` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `IsRead` int(11) DEFAULT NULL
+)
+
+--
+-- Dumping data for table `tblcontactus`
+--
+
+INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `PostingDate`, `AdminRemark`, `LastupdationDate`, `IsRead`) VALUES
+(1, 'Salekin', 's@gmail.com', 01751264433, ' This is sample text for the test.', '2019-08-29 19:03:08', 'Test for Admin Remark', '2019-08-30 12:55:23', 1);
+-- --------------------------------------------------------
+
+-
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
+(1, 'Salekin', 'Mirpur', 'Dhaka', 'Males', 'salekin@gmail.com', '1234567', '2019-08-30 07:03:09', '2019-08-30 11:59:05');
+-- Indexes for dumped tables
+--
